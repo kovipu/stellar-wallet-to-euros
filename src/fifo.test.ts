80be8832b00e818046d5a7ab3f0268d1a8bf4caa
Konsta Purtsi <konsta@purtsi.fi>
2025-07-16 17:39:15 +0300

    Parse payment

--- src/fifo.test.ts
+++ src/fifo.test.ts
@@ -1,27 +1,98 @@
 import { describe, expect, it } from "vitest";
 import { processTransactions } from "./fifo";
 
-
 describe("processTransactions", () => {
-    it("should process a create_account transaction", async () => {
-        const accountId = "GC7...";
+  it("should process a create_account transaction", async () => {
+    const accountId = "GC7...";
 
-        const mockOperations: any = [
-            {
-                type: "create_account",
-                starting_balance: "5.0000000",
-                created_at: "2024-01-01T00:00:00Z",
-                funder: "GBX...",
-            },
-        ];
-        const {transactions} = await processTransactions(mockOperations, accountId);
+    const mockOperations: any = [
+      {
+        type: "create_account",
+        starting_balance: "5.0000000",
+        created_at: "2024-01-01T00:00:00Z",
+        funder: "GBX...",
+      },
+    ];
+    const { transactions } = await processTransactions(
+      mockOperations,
+      accountId,
+    );
 
-        expect(transactions).toHaveLength(1);
-        expect(transactions[0].type).toBe("create_account");
-        expect(transactions[0].amountStroops).toBe(50_000_000n);
-        expect(transactions[0].currency).toBe("XLM");
-        expect(transactions[0].date).toStrictEqual(new Date("2024-01-01T00:00:00Z"));
-        expect(transactions[0].fromAddress).toBe("GBX...");
-        expect(transactions[0].toAddress).toBe(accountId);
-    });
+    expect(transactions).toHaveLength(1);
+    expect(transactions[0].type).toBe("create_account");
+    expect(transactions[0].amountStroops).toBe(50_000_000n);
+    expect(transactions[0].currency).toBe("XLM");
+    expect(transactions[0].date).toStrictEqual(
+      new Date("2024-01-01T00:00:00Z"),
+    );
+    expect(transactions[0].fromAddress).toBe("GBX...");
+    expect(transactions[0].toAddress).toBe(accountId);
+  });
+
+  it("should process a mix of create_account and payment operations", async () => {
+    const accountId = "GC7...";
+
+    const mockOperations: any = [
+      {
+        type: "create_account",
+        starting_balance: "1000.0000000",
+        created_at: "2024-01-01T00:00:00Z",
+        funder: "GBX...",
+      },
+      {
+        type: "payment",
+        from: accountId,
+        to: "GAZ...",
+        amount: "100.0000000",
+        asset_type: "native",
+        created_at: "2024-01-02T00:00:00Z",
+      },
+      {
+        type: "payment",
+        from: "GAZ...",
+        to: accountId,
+        amount: "50.0000000",
+        asset_type: "credit_alphanum4",
+        asset_code: "USDC",
+        created_at: "2024-01-03T00:00:00Z",
+      },
+    ];
+    const { transactions } = await processTransactions(
+      mockOperations,
+      accountId,
+      {},
+    );
+
+    expect(transactions).toHaveLength(3);
+
+    // create_account
+    expect(transactions[0].type).toBe("create_account");
+    expect(transactions[0].amountStroops).toBe(1000_0000000n);
+    expect(transactions[0].currency).toBe("XLM");
+    expect(transactions[0].date).toStrictEqual(
+      new Date("2024-01-01T00:00:00Z"),
+    );
+    expect(transactions[0].fromAddress).toBe("GBX...");
+    expect(transactions[0].toAddress).toBe(accountId);
+
+    // sent payment
+    expect(transactions[1].type).toBe("payment_sent");
+    expect(transactions[1].amountStroops).toBe(100_0000000n);
+    expect(transactions[1].currency).toBe("XLM");
+    expect(transactions[1].date).toStrictEqual(
+      new Date("2024-01-02T00:00:00Z"),
+    );
+    expect(transactions[1].fromAddress).toBe(accountId);
+    expect(transactions[1].toAddress).toBe("GAZ...");
+
+    // received payment
+    expect(transactions[2].type).toBe("payment_received");
+    expect(transactions[2].amountStroops).toBe(50_0000000n);
+    expect(transactions[2].currency).toBe("USDC");
+    expect(transactions[2].date).toStrictEqual(
+      new Date("2024-01-03T00:00:00Z"),
+    );
+    expect(transactions[2].fromAddress).toBe("GAZ...");
+    expect(transactions[2].toAddress).toBe(accountId);
+  });
 });
--- src/fifo.ts
+++ src/fifo.ts
@@ -21,7 +21,12 @@ async function main() {
       .order("asc")
       .call();
 
-    const output = await processTransactions(transactions, walletAddress);
+    // TODO: snapshot for testing.
+    const snapshot = transactions.slice(0, 4);
+
+    console.log(snapshot);
+
+    const output = await processTransactions(snapshot, walletAddress);
     exportToCsv(output);
   } catch (error) {
     console.error("Error:", (error as Error).message);
@@ -39,7 +44,8 @@ type WalletTransaction = {
   date: Date;
   type:
     | "create_account"
-    | "payment"
+    | "payment_received"
+    | "payment_sent"
     | "swap"
     | "swap_fee"
     | "blend_deposit"
@@ -53,56 +59,64 @@ type WalletTransaction = {
 export async function processTransactions(
   transactions: Horizon.ServerApi.OperationRecord[],
   walletAddress: string,
-): Output {
+): Promise<Output> {
   // Take the incoming transactions, and increment that currency's account's balance
-  return transactions.reduce((acc, tx) => {
-    switch (tx.type) {
-      case "create_account":
-        acc.transactions.push({
-          transactionHash: tx.transaction_hash,
-          date: new Date(tx.created_at),
-          type: "create_account",
-          fromAddress: tx.funder,
-          toAddress: walletAddress,
-          amountStroops: toStroops(tx.starting_balance),
-          currency: "XLM",
-        });
-        break;
-    }
-    return acc;
-  }, { transactions: [] } as Output);
-}
-
-if (require.main === module) {
-  main();
+  return transactions.reduce(
+    (acc, tx) => {
+      switch (tx.type) {
+        case "create_account":
+          acc.transactions.push({
+            transactionHash: tx.transaction_hash,
+            date: new Date(tx.created_at),
+            type: "create_account",
+            fromAddress: tx.funder,
+            toAddress: walletAddress,
+            amountStroops: toStroops(tx.starting_balance),
+            currency: "XLM",
+          });
+          break;
+        case "payment":
+          acc.transactions.push({
+            transactionHash: tx.transaction_hash,
+            date: new Date(tx.created_at),
+            type: tx.to === walletAddress ? "payment_received" : "payment_sent",
+            fromAddress: tx.from,
+            toAddress: tx.to,
+            amountStroops: toStroops(tx.amount),
+            currency:
+              tx.asset_type === "native" ? "XLM" : (tx.asset_code as Currency),
+          });
+          break;
+      }
+      return acc;
+    },
+    { transactions: [] } as Output,
+  );
 }
 
 // Convert amount to BigInt stroops.
 const toStroops = (amount: string): BigInt => {
   return BigInt(amount.replace(".", "").replace(",", ""));
-}
+};
 
 const exportToCsv = (output: Output) => {
   const outputCsv = stringify(
-    output.transactions.map(tx => ({
-      "Date": tx.date.toISOString(),
-      "Type": tx.type,
-      "From": tx.fromAddress,
-      "To": tx.toAddress,
-      "Amount": tx.amountStroops,
-      "Currency": tx.currency,
+    output.transactions.map((tx) => ({
+      Date: tx.date.toISOString(),
+      Type: tx.type,
+      From: tx.fromAddress,
+      To: tx.toAddress,
+      Amount: tx.amountStroops,
+      Currency: tx.currency,
     })),
     {
       header: true,
-      columns: [
-        "Date",
-        "Type",
-        "From",
-        "To",
-        "Amount",
-        "Currency",
-      ],
-    }
+      columns: ["Date", "Type", "From", "To", "Amount", "Currency"],
+    },
   );
   console.log(outputCsv);
+};
+
+if (require.main === module) {
+  main();
 }
